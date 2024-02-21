dcae035409913f60dc69e3f9767263dfcfd7dfd2
zhang jun <z0413j@outlook.com>
2024-02-21 17:55:30 +0800

    upgrade dependencies

--- go.mod
+++ go.mod
@@ -3,18 +3,23 @@ module github.com/mroject/freeswitch_exporter
 go 1.18
 
 require (
-	github.com/prometheus/client_golang v1.12.2
+	github.com/prometheus/client_golang v1.17.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
 
 require (
+	github.com/alecthomas/kingpin/v2 v2.4.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
-	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
-	google.golang.org/appengine v1.6.6 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
@@ -22,16 +27,16 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/go-kit/log v0.2.1
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.34.0
-	github.com/prometheus/exporter-toolkit v0.7.3
-	github.com/prometheus/procfs v0.7.3 // indirect
-	golang.org/x/net v0.0.0-20220921203646-d300de134e69
-	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
-	golang.org/x/text v0.3.8
-	google.golang.org/protobuf v1.30.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
+	github.com/prometheus/common v0.45.0
+	github.com/prometheus/exporter-toolkit v0.11.0
+	github.com/prometheus/procfs v0.11.1 // indirect
+	golang.org/x/net v0.21.0
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
@@ -35,10 +33,10 @@ func main() {
 			"web.config",
 			"[EXPERIMENTAL] Path to config yaml file that can enable TLS or authentication.",
 		).Default("").String()
-		rtpEnable = kingpin.Flag("rtp.enable", "enable rtp info, default: false").Default("false").Bool()
+		rtpEnable = kingpin.Flag("rtp.enable", "enable rtp info(feature:todo!), default: fasle").Default("false").Bool()
 	)
 	promlogConfig := &promlog.Config{}
-	kingpin.Version("freeswitch_exporter")
+	kingpin.Version("freeswitch_exporter\nversion: 1.0.4")
 	logger := promlog.New(promlogConfig)
 	kingpin.Parse()
 
@@ -51,35 +49,6 @@ func main() {
 	prometheus.MustRegister(c)
 
 	http.Handle(*metricsPath, promhttp.Handler())
-
-	// This implements Prometheus' multi-target exporter support
-	// Example project: Official Blackbox Exporter
-	// https://github.com/prometheus/blackbox_exporter#prometheus-configuration
-	http.HandleFunc("/probe", func(w http.ResponseWriter, r *http.Request) {
-		target := r.URL.Query().Get("target")
-		if target == "" {
-			http.Error(w, "'target' query param not provided, but required.", http.StatusBadRequest)
-
-		}
-
-		// Not checking for the port to allow the port to be configured in
-		// the Prometheus scrape target config.
-		if !strings.HasPrefix(target, "tcp://") {
-			target = fmt.Sprintf("tcp://%s", target)
-		}
-
-		c, colErr := NewCollector(target, *timeout, *password, *rtpEnable)
-		if colErr != nil {
-			http.Error(w, fmt.Sprintf("failed to create collector for %s: %s", target, colErr), http.StatusInternalServerError)
-		}
-
-		registry := prometheus.NewRegistry()
-		registry.MustRegister(c)
-
-		promhttp.HandlerFor(registry, promhttp.HandlerOpts{}).ServeHTTP(w, r)
-
-	})
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
 			<head><title>FreeSWITCH Exporter</title></head>
@@ -90,9 +59,13 @@ func main() {
 			</html>`))
 	})
 
-	level.Info(logger).Log("msg", "Listening on", "address", *listenAddress)
 	server := &http.Server{Addr: *listenAddress}
-	if err := web.ListenAndServe(server, *configFile, logger); err != nil {
+	flags := &web.FlagConfig{
+		WebListenAddresses: &[]string{*listenAddress},
+		WebSystemdSocket:   new(bool),
+		WebConfigFile:      configFile,
+	}
+	if err := web.ListenAndServe(server, flags, logger); err != nil {
 		level.Info(logger).Log("err", err)
 		os.Exit(1)
 	}
