58aa1c4849c8714aedae50cca82f5ca7e658122c
bmcodez <bardhmaxhera5@gmail.com>
2025-06-04 16:43:59 +0200

    Update collector.go
    added current conference count

--- collector.go
+++ collector.go
@@ -189,6 +189,7 @@ type Verto struct {
 var (
 	metricList = []Metric{
 		{Name: "current_calls", Type: prometheus.GaugeValue, Help: "Number of calls active", Command: "api show calls count as json"},
+		{Name: "current_conferences", Type: prometheus.GaugeValue, Help: "Number of active conferences", Command: "api conference count as json"},
 		{Name: "detailed_bridged_calls", Type: prometheus.GaugeValue, Help: "Number of detailed_bridged_calls active", Command: "api show detailed_bridged_calls as json"},
 		{Name: "detailed_calls", Type: prometheus.GaugeValue, Help: "Number of detailed_calls active", Command: "api show detailed_calls as json"},
 		{Name: "bridged_calls", Type: prometheus.GaugeValue, Help: "Number of bridged_calls active", Command: "api show bridged_calls as json"},
@@ -794,6 +795,12 @@ func (c *Collector) fetchMetric(metricDef *Metric) (float64, error) {
 			return 0, fmt.Errorf("cannot read JSON response: %w", err)
 		}
 
+		return r.Count, nil
+	case "current_conferences":
+		r := struct {
+			Count float64 `json:"row_count"`
+		}{}
+		r.Count = 12
 		return r.Count, nil
 	case "current_channels":
 		r := struct {
